9a34291b8d1c2ed25ffe796bc0ce82690b5d0dc3
toBeOfUse <mitchjacov@gmail.com>
2022-07-12 13:27:53 -0400

    separate index and portfolio pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,43 @@
 <template>
-  <PortfolioHeader />
+  <div class="max-w-screen-lg mx-auto">
+    <nuxt-picture
+      quality="85"
+      width="1920"
+      height="444"
+      preload
+      src="/wide.jpg"
+      :img-attrs="{ class: 'w-full h-auto' }"
+    />
+    <h1 class="text-5xl">Mitch Jacovetty</h1>
+    <ul class="list-disc pl-8 my-4">
+      <li class="text-2xl">Born 1997</li>
+      <li class="text-2xl">Programmer and Som'times-Do-Well</li>
+      <li class="text-2xl">Aspiring Towards Greatness</li>
+      <li class="text-2xl"><a href="/portfolio">My Portfolio</a></li>
+      <li class="text-2xl">
+        <a href="https://github.com/toBeOfUse">My Github</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
-
 export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
 
-<style>
-body {
-  background: linear-gradient(
-    0deg,
-    rgba(0, 0, 0, 0.05) 0%,
-    rgba(0, 0, 0, 0.15) 100%
-  );
-  background-repeat: no-repeat;
-  background-attachment: fixed;
+<style scoped>
+li,
+h1,
+p {
+  margin-top: 10px;
+}
+ul {
+  list-style-type: ' - ';
+}
+a {
+  text-decoration: underline;
 }
 </style>
---
+++ pages/portfolio.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-6">
+    <PortfolioHeader />
+    <portfolio-item :post="post" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import PortfolioItem from '~/components/PortfolioItem.vue'
+
+export default Vue.extend({
+  name: 'PortfolioPage',
+  components: { PortfolioItem },
+  async asyncData({ $content }) {
+    return {
+      post: await $content('projects/perspective').fetch(),
+    }
+  },
+})
+</script>
+
+<style>
+body {
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0.05) 0%,
+    rgba(0, 0, 0, 0.15) 100%
+  );
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+}
+</style>
