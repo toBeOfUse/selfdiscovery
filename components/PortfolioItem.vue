b881f134fce3f7c786ec5f7c167ce81d033c5908
toBeOfUse <mitchjacov@gmail.com>
2022-07-14 14:09:53 -0400

    add links, iframes to PortfolioItem

--- components/PortfolioItem.vue
+++ components/PortfolioItem.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="flex flex-col max-w-screen-md items-center mx-auto">
     <hr class="w-full m-4 border-t-2 border-gray-400" />
-    <h1 class="text-xl my-4">{{ post.title }}</h1>
+    <div class="flex items-end py-4">
+      <h1 :id="post.slug" class="text-xl mx-4">{{ post.title }}</h1>
+      <a
+        v-if="post.github_link"
+        class="mx-2 text-sm"
+        :href="post.github_link"
+        _target="blank"
+        >Github</a
+      >
+      <span v-if="post.live_link && post.github_link">•</span>
+      <a
+        v-if="post.live_link"
+        class="mx-2 text-sm"
+        :href="post.live_link"
+        _target="blank"
+        >Live</a
+      >
+    </div>
     <nuxt-content :document="post" class="max-w-screen-md prose text-black" />
-    <img :src="post.image" :alt="post.image_alt" />
+    <img v-if="post.image" :src="post.image" :alt="post.image_alt" />
+    <iframe
+      v-if="post.iframe"
+      :src="post.iframe"
+      class="w-full"
+      style="height: 50vh"
+      loading="lazy"
+    />
   </div>
 </template>
 
 <script lang="ts">
+// TODO: directional quation marks?
 import Vue from 'vue'
 export default Vue.extend({
   name: 'PortfolioItem',
@@ -19,3 +44,8 @@ export default Vue.extend({
   },
 })
 </script>
+<style scoped>
+a {
+  text-decoration: underline;
+}
+</style>
