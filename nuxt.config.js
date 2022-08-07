bca5decf20a273e15794474bb59f420c9778618f
toBeOfUse <mitchjacov@gmail.com>
2022-08-07 03:48:36 -0400

    use vue svg loader instead of having Logo.vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,18 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    extend: (config) => {
+      const svgRule = config.module.rules.find((rule) => rule.test.test('.svg'))
+
+      svgRule.test = /\.(png|jpe?g|gif|webp)$/
+
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['babel-loader', 'vue-svg-loader'],
+      })
+    },
+  },
   image: {},
   tailwindcss: { config: { plugins: [require('@tailwindcss/typography')] } },
 }
