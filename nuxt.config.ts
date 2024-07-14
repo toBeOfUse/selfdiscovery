c541794c75fbf68ba9f33e49dc92e879e05cfa4f
Mitch J <mjacovet@kent.edu>
2024-07-13 21:43:57 -0400

    update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  nitro: {
+    prerender: {
+      routes: ["/"]
+    }
+  },
+  // experimental: {
+  //   payloadExtraction: false
+  // },
   modules: ["@nuxtjs/tailwindcss", "nuxt-svgo", "@nuxt/content", "@nuxt/image"],
+
   svgo: {
     svgo: false,
     defaultImport: "component",
   },
+
   app: {
     head: {
       title: "Mitch's Web Page",
@@ -15,7 +25,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { name: "format-detection", content: "telephone=no" },  // tf
+        { name: "format-detection", content: "telephone=no" }, // tf
         {
           property: "og:description",
           hid: "og:description",
@@ -30,4 +40,6 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/png", href: "/micon.png" }],
     },
   },
+
+  compatibilityDate: "2024-07-13",
 });
