97b0abbcb0d4e2e46016b0a4e214f55ee9810a8d
Mitch J <mjacovet@kent.edu>
2024-09-15 00:39:33 -0400

    add comments with future "projects" entries

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,21 +2,13 @@
   <NuxtLayout name="projects">
     <div>
       <h1 class="text-3xl text-center" style="font-variant: small-caps">
-        <NuxtLink to="/projects/" class="no-underline">
-          What Have I Done?
-        </NuxtLink>
+        <NuxtLink to="/projects/" class="no-underline"> What Have I Done? </NuxtLink>
       </h1>
       <p>
-        This is the question we must all ask ourselves from time to time, and
-        this is the page where I'm recording my answers. Also, hi, I'm
-        <NuxtLink class="underline" to="/about">some kind of programmer</NuxtLink>.
-        To learn more, read on, and/or
-        <a class="underline" href="mailto:mitchjacov@gmail.com">email me</a>.
+        This is the question we must all ask ourselves from time to time, and this is the page where I'm recording my answers. Also, hi, I'm
+        <NuxtLink class="underline" to="/about">some kind of programmer</NuxtLink>. To learn more, read on, and/or <a class="underline" href="mailto:mitchjacov@gmail.com">email me</a>.
       </p>
-      <ProjectItem
-        v-for="post, i in posts" :key="orderedPostNames[i]" :name="orderedPostNames[i]"
-        :post="post"
-      />
+      <ProjectItem v-for="(post, i) in posts" :key="orderedPostNames[i]" :name="orderedPostNames[i]" :post="post" />
     </div>
   </NuxtLayout>
 </template>
@@ -35,17 +27,14 @@ const orderedPostNames = [
   "catullus",
   "corn-simulator",
   "minecraft-maps",
+  // "twitter-archive-browser",
+  // scattered single pages - progress meter, v-arcs, mail, etc.
   "presentations",
   "sweet-sh",
   "this-website",
 ];
 
-const { data: posts } = await useAsyncData(
-  "projects",
-  () => Promise.all(
-    orderedPostNames.map(n => queryContent("/projects", n).findOne())
-  )
-);
+const { data: posts } = await useAsyncData("projects", () => Promise.all(orderedPostNames.map((n) => queryContent("/projects", n).findOne())));
 
 useHead({
   title: "Projects",
