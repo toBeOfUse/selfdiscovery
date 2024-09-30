0b9dbdb2673384d377f091b06a9259260e7a15ca
Mitch J <mjacovet@kent.edu>
2024-09-30 02:18:25 -0400

    extract "above the cut" from markdown as descriptions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,15 @@ import vue from "@astrojs/vue";
 import robotsTxt from "astro-robots-txt";
 
 import sitemap from "@astrojs/sitemap";
+import { getDescription } from "./remark-readmore-description.mjs";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mitch.website",
   integrations: [tailwind(), vue(), robotsTxt(), sitemap()],
+  markdown: {
+    remarkPlugins: [getDescription],
+  },
   vite: {
     css: {
       preprocessorOptions: {
---
+++ remark-readmore-description.mjs
@@ -0,0 +1,18 @@
+import { toString } from "mdast-util-to-string";
+
+export function getDescription() {
+  /**
+   * @param {import('mdast').Parent} tree
+   */
+  return function (tree, file) {
+    for (let i = 0; i < tree.children.length; ++i) {
+      console.log(tree.children[i]);
+      if (/<!--\s*more\s*-->/.test(tree.children[i].value)) {
+        const childrenBeforeCut = { ...tree, children: tree.children.slice(0, i) };
+        const textBeforeCut = toString(childrenBeforeCut);
+        file.data.astro.frontmatter.description = textBeforeCut;
+        return;
+      }
+    }
+  };
+}
