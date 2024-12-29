623eb45e54ce4db967dc84327e7280a6b67f5e2f
Mitch J <mjacovet@kent.edu>
2024-12-29 08:09:54 -0500

    gradually expand posts

--- src/components/ShowMore.vue
+++ src/components/ShowMore.vue
@@ -1,16 +1,45 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const open = ref(false);
+import { ref, nextTick } from "vue";
+import { TransitionPresets, executeTransition } from "@vueuse/core";
+
+const isOpen = ref(false);
+const intro = ref<HTMLDivElement | null>(null);
+const full = ref<HTMLDivElement | null>(null);
+const height = ref(0);
+
+const open = async () => {
+  isOpen.value = true;
+  if (intro.value) {
+    const introHeight = intro.value.getBoundingClientRect().height;
+    await nextTick();
+    // await new Promise((r) => requestAnimationFrame(r));
+    if (full.value) {
+      const fullHeight = full.value.getBoundingClientRect().height;
+      await executeTransition(height, introHeight, fullHeight, {
+        duration: 1000,
+        transition: TransitionPresets.linear,
+      });
+      height.value = 0;
+    }
+  }
+};
 </script>
 
 <template>
-  <!-- TODO: "opening" transition animation (will require measuring) -->
-  <template v-if="open">
-    <slot name="all" />
+  <template v-if="isOpen">
+    <div
+      style="overflow: hidden"
+      :style="{ height: height !== 0 ? height + 'px' : undefined }"
+      ref="full"
+    >
+      <slot name="all" />
+    </div>
   </template>
   <template v-else>
-    <slot name="intro" />
-    <button @click="open = true" class="bg-transparent p-2 border border-gray-300">
+    <div ref="intro">
+      <slot name="intro" />
+    </div>
+    <button @click="open" class="bg-transparent p-2 border border-gray-300">
       Show More
     </button>
   </template>
