b36ff9e2701f51a4719f0123d62f9b3329aad7cb
Mitch J <mjacovet@kent.edu>
2025-03-18 10:24:09 -0400

    create md processor w/ config so images work

--- src/md.ts
+++ src/md.ts
@@ -1,5 +1,8 @@
 import { createMarkdownProcessor } from "@astrojs/markdown-remark";
-const { render } = await createMarkdownProcessor();
+import config from "../astro.config.mjs";
+// @ts-ignore there are tiny differences between the version of shiki in the
+// config and the version createMarkdownProcessor expects, for some reason
+const { render } = await createMarkdownProcessor(config.markdown);
 
 // thing to render markdown into HTML. i think it will even use plugins and
 // stuff. this is useful to render the "preview" of blog posts that is extracted
